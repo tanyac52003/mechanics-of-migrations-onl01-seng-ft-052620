a9d9ec2e868f803fc10f25c10d222f3fa46f146f
Tanya Hodges <tanyac52003@gmail.com>
2020-06-27 23:29:09 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -5,9 +5,12 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   def down
   end 
   
-  def change 
-  end
-  
-  ActiveRecord::Base.establish_connection
-  
+  def change
+    create_table :artists do |t|
+      t.string :name 
+      t.string :genre 
+      t.integer :age 
+      t.string :hometown
+    end 
+  end 
 end 
